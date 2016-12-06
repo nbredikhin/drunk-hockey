7bf06fcefc8e644ae822d9a161b372784a8d0286
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2016-12-07 01:30:48 +0300

    Изменены кнопки, анимация меню
    - исправлена текстура кнопок
    - добавлена анимация кнопок и логотипа

---
+++ Assets/scripts/menu/Menu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    void SingleplayerButtonClick()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    void MultiplayerButtonClick()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    void SettingsButtonClick()
+    {
+
+    }
+}
---
+++ Assets/scripts/menu/MenuLogo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MenuLogo : MonoBehaviour
+{
+	public float maxSlideDistance = 1f;
+	public float slideSpeed = 1.5f;
+
+	private RectTransform rectTransform;
+	private Vector3 initialPosition;
+
+    void Start()
+    {
+		rectTransform = GetComponent<RectTransform>();
+		initialPosition = rectTransform.position;
+    }
+
+    void Update()
+    {
+		rectTransform.position = initialPosition + new Vector3(0, Mathf.Sin(Time.time * slideSpeed) * maxSlideDistance, 0);
+    }
+}
