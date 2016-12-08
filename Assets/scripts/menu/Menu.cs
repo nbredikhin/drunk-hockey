acc1151f5c947bee76657bf2966b0e63edd9c61a
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2016-12-08 22:35:04 +0300

    Добавлена переменная, определяющая тип игры
    - исправлено форматирование

--- Assets/scripts/menu/Menu.cs
+++ Assets/scripts/menu/Menu.cs
@@ -3,14 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-
     public void SingleplayerButtonClick()
     {
+        GameMain.isMultiplayerGame = false;
         SceneManager.LoadScene("Game");
     }
 
     public void MultiplayerButtonClick()
     {
+        GameMain.isMultiplayerGame = true;
         SceneManager.LoadScene("Game");
     }
 }
--- Assets/scripts/menu/MenuLogo.cs
+++ Assets/scripts/menu/MenuLogo.cs
@@ -2,20 +2,20 @@
 
 public class MenuLogo : MonoBehaviour
 {
-	public float maxSlideDistance = 1f;
-	public float slideSpeed = 1.5f;
+    public float maxSlideDistance = 1f;
+    public float slideSpeed = 1.5f;
 
-	private RectTransform rectTransform;
-	private Vector3 initialPosition;
+    private RectTransform rectTransform;
+    private Vector3 initialPosition;
 
     void Start()
     {
-		rectTransform = GetComponent<RectTransform>();
-		initialPosition = rectTransform.position;
+        rectTransform = GetComponent<RectTransform>();
+        initialPosition = rectTransform.position;
     }
 
     void Update()
     {
-		rectTransform.position = initialPosition + new Vector3(0, Mathf.Sin(Time.time * slideSpeed) * maxSlideDistance, 0);
+        rectTransform.position = initialPosition + new Vector3(0, Mathf.Sin(Time.time * slideSpeed) * maxSlideDistance, 0);
     }
 }
