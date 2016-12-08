3f6f8fdccf656e1511c5c00c6ee70b8634ea7f8c
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2016-12-09 01:29:59 +0300

    Тряска камеры при сильных ударах шайбы

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -5,10 +5,15 @@ public class CameraController : MonoBehaviour
     public float horizontalScale = 0.04f;
     public float verticalScale = 0.01f;
     private float initialRotation;
+    private Vector3 initialPosition;
+
+    private float shakePower = 0f;
+    private float shakeFadeMul = 0.87f;
 
     void Start()
     {
         initialRotation = transform.eulerAngles.z;
+        initialPosition = transform.position;
 
         var area = GameObject.Find("area");
         var areaSprite = area.GetComponent<SpriteRenderer>();
@@ -24,5 +29,13 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         transform.rotation = Quaternion.Euler(0f, 0f, initialRotation + Mathf.Sin(Time.time));
+        transform.position = initialPosition + new Vector3((Random.value - 0.5f) * shakePower, (Random.value - 0.5f) * shakePower, 0f);
+
+        shakePower = shakePower * shakeFadeMul;
+    }
+
+    public void Shake(float power)
+    {
+        shakePower = power;
     }
 }
---
+++ Assets/scripts/PuckController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PuckController : MonoBehaviour
+{
+    // Сила столкновения, при котором начинает трястись камера
+    public float cameraShakeCollision = 4f;
+
+    private CameraController cameraController;
+
+    void Start()
+    {
+        cameraController = GameObject.Find("Main Camera").GetComponent<CameraController>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.magnitude > cameraShakeCollision)
+        {
+            cameraController.Shake(0.03f);
+        }
+    }
+}
