6870ec80d2aee276b76d86a1137937cde9e610f4
Evgeniy Morozov <zhe3145@yandex.ru>
2017-02-09 01:15:03 +0300

    Сейвы и броски в створ

--- game/Puck.lua
+++ game/Puck.lua
@@ -26,16 +26,24 @@ local function collision(self, event)
             end
         end
 
+        if scene and (event.other.isPlayer or event.other.isGates) then
+            if self:isPlayerShotOnGoal() then
+                DEBUG.Log("SHOT ON GOAL: %d", self.lastTouchingPlayer.goalShots)
+                self.lastTouchingPlayer.goalShots = self.lastTouchingPlayer.goalShots + 1
+            end
+        end
+
         if scene and event.other.isPlayer then
             self.lastTouchingPlayer = event.other
-            if self.shotOnGoal then
-                local toPlayerX, toPlayerY = self.lastTouchingPlayer.x - self.x, self.lastTouchingPlayer.y - self.y;
-                local vx, vy = self:getLinearVelocity()
+
+            if self:isShotOnGoal() then
+                local toPlayerX, toPlayerY = event.other.x - self.x, event.other.y - self.y;
+                local vx, vy = self.prevVx, self.prevVy
                 local indicator = toPlayerX * vx + toPlayerY * vy
                 DEBUG.Log("Save by " ..  event.other.colorName)
                 if indicator > 0 then
                     scene:showGameText("save", self.x, self.y, event.other.colorName)
-                    self.lastTouchingPlayer.savesCount = self.lastTouchingPlayer.savesCount + 1
+                    event.other.savesCount = event.other.savesCount + 1
                     self.shotOnGoal = false
                 end
             end
@@ -46,37 +54,64 @@ local function collision(self, event)
         end
     elseif event.phase == "ended" then
         self.angularVelocity = (math.random() - 0.5) * 1000
+        -- Обновим скорости
+        self.prevVx, self.prevVy = self:getLinearVelocity()
     end
 end
 
-local function checkShotOnGoal(self)
-    local vx, vy = self:getLinearVelocity()
+local function isPlayerShotOnGoal(self)
+    local gates = self:getGatesInDirection()
+    if not gates then
+        DEBUG.Log("Player shot on goal: false")
+        return false
+    end
+
+    if gates.colorName == self.lastTouchingPlayer.colorName then
+        DEBUG.Log("Player shot on goal: false")
+        return false
+    end
+    DEBUG.Log("Player shot on goal: true")
+    return true
+end
+
+local function isShotOnGoal(self)
+    local gates = self:getGatesInDirection()
+    if gates == nil then
+        DEBUG.Log("Shot on goal: false")
+        return false
+    end
+    DEBUG.Log("Shot on goal: true")
+    return true
+end
+
+local function getGatesInDirection(self)
+    local vx, vy = self.prevVx, self.prevVy
     local hits = physics.rayCast(self.x, self.y, self.x + vx * RAYCAST_DISTANCE_MUL,
-        self.y + vy * RAYCAST_DISTANCE_MUL, "unsorted")
+        self.y + vy * 1, "unsorted")
     if hits then
         local gate
         for i, hit in ipairs(hits) do
             if hit.object.isGate then
+                DEBUG.Log("OH YEAH")
                 gate = hit.object
                 break
             end
         end
         if not gate then
-            return false
+            return nil
         end
-        if gate.y < display.contentCenterY / 2 and self.y < gate.y + gate.height * 2 or
-           gate.y > display.contentCenterY / 2 and self.y > gate.y - gate.height * 2
+        if gate.y < display.contentCenterY / 2 and self.y < gate.y - gate.height * 2 or
+           gate.y > display.contentCenterY / 2 and self.y > gate.y + gate.height * 2
         then
-            return false
+            DEBUG.Log("OH NO")
+            return nil
         end
-        DEBUG.Log("Shot on goal!!!")
-        return true
+        return gate
     end
-    return false
+    return nil
 end
 
 local function update(self)
-    self.shotOnGoal = self:checkShotOnGoal()
 end
 
 local function constructor()
@@ -90,15 +125,19 @@ local function constructor()
         friction = 0,
         radius = 2.7
     })
+    self.prevVx = 0
+    self.prevVy = 0
+
     self.linearDamping = 0.2
     self.angularDamping = 5
     self.isPuck = true
-    self.shotOnGoal = false
 
     -- Methods
     self.collision       = collision
     self.update          = update
-    self.checkShotOnGoal = checkShotOnGoal
+    self.isShotOnGoal    = isShotOnGoal
+    self.isPlayerShotOnGoal  = isPlayerShotOnGoal
+    self.getGatesInDirection = getGatesInDirection
 
     self:addEventListener("collision")
     return self
